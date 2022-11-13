c1be8e356a8ed9618bb55388a48a9e9c1fedf9fe
Kovács Bertalan <79515988+bartieplatan@users.noreply.github.com>
2022-11-13 11:04:32 +0100

    Add files via upload

--- backend/main.py
+++ backend/main.py
@@ -1,17 +1,62 @@
 from flask import *
 import json
+import os
 import sqlite3
+from werkzeug.utils import secure_filename
 from flask_cors import CORS
 
+UPLOAD_FOLDER = '/path/to/the/uploads'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1000 * 1000
 CORS(app)
+app.add_url_rule(
+    "/uploads/<name>", endpoint="download_file", build_only=True
+)
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/uploads/<imageName>')
+def download_file(name):
+    return send_from_directory(app.config["UPLOAD_FOLDER"], name)
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('download_file', name=filename))
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
 
 @app.route("/view")
 def view():
     con = sqlite3.connect("employee.db")
     con.row_factory = sqlite3.Row
     cur = con.cursor()
-    cur.execute("select * from Employees")
+    cur.execute("select * from Employees2")
     rows = cur.fetchall()
     return json.dumps([dict(ix) for ix in rows])
 
@@ -24,9 +69,12 @@ def saveDetails():
         name = data["name"]
         email = data["email"]
         address = data["address"]
+        nickname = data["nickname"]
+        image = data["image"]
+        print(data["image"])
         with sqlite3.connect("employee.db") as con:
             cur = con.cursor()
-            cur.execute("INSERT into Employees (name, email, address) values (?,?,?)", (name, email, address))
+            cur.execute("INSERT into Employees2 (name, email, address, nickname, image) values (?,?,?,?,?)", (name, email, address, nickname, image))
             con.commit()
             msg = "Employee successfully Added"
     except:
@@ -44,7 +92,7 @@ def deleterecord():
     with sqlite3.connect("employee.db") as con:
         try:
             cur = con.cursor()
-            cur.execute("delete from Employees where id = ?", id)
+            cur.execute("delete from Employees2 where id = ?", id)
             msg = "record successfully deleted"
         except:
             msg = "can't be deleted"
@@ -58,10 +106,11 @@ def updaterecord():
         name = data["name"]
         email = data["email"]
         address = data["address"]
+        nickname = data["nickname"]
 
         with sqlite3.connect("employee.db") as con:
             cur = con.cursor()
-            cur.execute("UPDATE Employees SET name=?, email=?, address=? WHERE id=?", (name, email, address, id))
+            cur.execute("UPDATE Employees2 SET name=?, email=?, address=?, nickname=? WHERE id=?", (name, email, address, nickname, id))
             con.commit()
             msg = "Employee successfully Updated"
     except:
