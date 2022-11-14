ce31d7922e0b73e73616d7e7f457916e812a17dd
Kovács Bertalan <79515988+bartieplatan@users.noreply.github.com>
2022-11-14 21:11:22 +0100

    Add files via upload

--- js/add.js
+++ js/add.js
@@ -4,16 +4,16 @@ function sendPost(){
         email: document.getElementById("email").value,
         address:document.getElementById("address").value,
         nickname:document.getElementById("nickname").value,
-        image:document.getElementById("image").src
+        //image:document.getElementById("image").src
       });
-      console.log(image);
+      console.log(data.image);
       sendImage(image);
       navigator.sendBeacon('http://127.0.0.1:5000/savedetails/', data);
       console.log(data);
 
     }
-function sendImage(imageName){
-    console.log(imageName);
-    navigator.sendBeacon('http://127.0.0.1:5000/uploads/<imageName>/');
+function sendImage(name){
+    console.log(name);
+    navigator.sendBeacon('http://127.0.0.1:5000/images/');
 
 }
